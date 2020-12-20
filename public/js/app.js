2ef55da7250464b69fa5b896dd19518fcdd4b0a5
Vladislav <vlad.markotin@gmail.com>
2020-12-20 19:13:21 +0300

    Change name of project.Add profile page prototype

--- public/js/app.js
+++ public/js/app.js
@@ -49752,6 +49752,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
+var i = 1;
 Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
