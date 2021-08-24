d02bb172e5cf82ff189009e41b4a1f7f7a50f5ad
3051285A035PB0 <3051285A035PB0@gmail.com>
2021-08-24 20:31:24 +0300

    redesign of PlanComponent and showing of alert

--- resources/js/components/PlanComponent.vue
+++ resources/js/components/PlanComponent.vue
@@ -1,219 +1,218 @@
 <template>
+    <v-card>
+        <v-card-title style="background-color: #A10000;color: white;">Create your day plan!</v-card-title>
+        <v-container>
 
-    <v-container >
-
-
-        <v-select
-              :items="dayStatuses"
-              label="Day status"
-              v-model="day_status"
-              required
-            ></v-select>
-            <v-divider></v-divider>
-
-    <div style="    margin-left: auto;
-                    margin-right: auto;
-                    ">
-
-
-      <v-row align="center" class="d-flex justify-start mb-6">
-        <v-col cols="1" md="1" v-if="defaultSelected.hash == '#'">
-            <v-tooltip left>
-
-                    <template v-slot:activator="{ on }">
-
-                             <div class="text-center">
-                                <v-dialog
-                                  v-model="dialog"
-                                  width="500"
-                                >
-                                  <template  v-slot:activator="{ on, attrs }">
-                                    <v-btn icon v-show="showIcon > 3"
-                                      color="red lighten-2"
-                                      dark
-                                      v-bind="attrs"
-                                      v-on="on"
-                                    >
-                                     <v-icon md="1"
-                                        color="#D71700"
-                                     > {{icons.mdiPlus}}
-
-                                     </v-icon>
-                                    </v-btn>
-                                  </template>
-
-                                  <v-card>
-                                    <v-card-title class="headline grey lighten-2" >
-                                      Add #code for this task
-                                    </v-card-title>
-
-                                    <v-card-text>
-                                      <v-text-field
-                                        v-model="newHashCode"
-                                        @keypress.enter = "addNewHashCode()"
-                                        placeholder="#"></v-text-field>
-                                    </v-card-text>
-
-                                    <v-divider></v-divider>
-
-                                    <v-card-actions>
-                                      <v-spacer></v-spacer>
-                                      <v-btn
-                                        color="#D71700"
-                                        text
-                                        @click = "addNewHashCode()"
-
-                                      >
-                                        Add
-                                      </v-btn>
-                                    </v-card-actions>
-                                  </v-card>
-                                </v-dialog>
-                              </div>
-                              </template>
-                               <span>Add hash-code to task for quick access</span>
-             </v-tooltip>
-
-
-        </v-col>
-        <v-col
-          cols="4"
-          md="1"
-        >
             <v-select
-                :items="defaultSelected.hashCodes"
-                value="defaultSelected.hashCodes[0]"
-                v-model="defaultSelected.hash"
-                @change="onChange"
-                required>
-
-            </v-select>
-        </v-col>
-        <v-col cols="4" md="3">
-            <v-text-field
-                :placeholder=" placeholders[0] "
-                :counter="25"
-                required
-                v-model="defaultSelected.taskName"
-                @input="inputChangeHandler"
-                @keypress.enter = "addTask"
-            ></v-text-field>
-            </v-col>
-            <v-col cols="1" md="2">
-            <v-select
-                   :label="placeholders[1]"
-                   required
-                   :items="taskTypes"
-                   v-model="defaultSelected.type"
-             ></v-select>
-            </v-col>
-            <v-col cols="1" md="1">
-                <v-select
-                                   :label="placeholders[2]"
-                                   required
-                                   :items="taskPriority"
-                                   v-model="defaultSelected.priority"
-
+                  :items="dayStatuses"
+                  label="Day status"
+                  v-model="day_status"
+                  required
                 ></v-select>
-            </v-col>
-            <v-col cols="1" md="1">
-                <v-text-field
-                      v-model="defaultSelected.time"
-                      label="Time for task"
-                    >00:00</v-text-field>
-            </v-col>
-            <v-col cols="1" md="1">
-                 <v-textarea
+                <v-divider></v-divider>
 
-                                     outlined
-                                     rows="2"
-                                     row-height="4"
-                                     v-model="defaultSelected.details"
-                                     shaped
-                           :placeholder="placeholders[4]"
-                           :counter="256"
-                 ></v-textarea>
-             </v-col>
-             <v-col cols="1" md="1">
-                <v-text-field
-                                :placeholder=" placeholders[5] "
-                                v-model="defaultSelected.notes"
-                                @keypress.enter = "addTask"
-                ></v-text-field>
-             </v-col>
-        <v-col cols="1" md="1">
-           <v-tooltip right>
-             <template v-slot:activator="{ on }">
-                <div v-on="on">
-                    <v-btn icon>
-                               <v-icon md="1"
-                                    color="#D71700"
-                                    v-on:click="addTask"> {{icons.mdiPlex}}
-                                </v-icon>
-                    </v-btn>
-                </div>
-                </template>
-                <span>Add task to day plan</span>
-            </v-tooltip>
-        </v-col>
-
-      </v-row>
-      </div>
-      <v-divider></v-divider>
-      <div>
-      <table>
-      <tr>
             <div>
-                <div style="display:'block';width:800px;margin:auto;">
-                    <v-data-table
-                        :headers="headers"
-                        :items="items"
-                        class="elevation-1"
-                        @dblclick:tr="deleteItem"
-                      >
-                        <template v-slot:item="props">
-                            <tr align="center"  ref="refWord">
-                                <td>{{ props.item.hash }}</td>
-                                <td>{{ props.item.taskName }}</td>
-                                <td>{{ props.item.type }}</td>
-                                <td>{{ props.item.priority }}</td>
-                                <td>{{ props.item.time }}</td>
-                                <td>{{ props.item.details }}</td>
-                                <td>{{ props.item.notes }}</td>
-                                <td><v-btn @click="deleteItem(props.item)">Delete task</v-btn></td>
-                            </tr>
+
+              <v-row align="center" class="d-flex mb-6">
+                <v-col cols="1" md="1" v-if="defaultSelected.hash == '#'">
+                    <v-tooltip left>
+
+                            <template v-slot:activator="{ on }">
+
+                                     <div class="text-center">
+                                        <v-dialog
+                                          v-model="dialog"
+                                          width="500"
+                                        >
+                                          <template  v-slot:activator="{ on, attrs }">
+                                            <v-btn icon v-show="showIcon > 3"
+                                              color="red lighten-2"
+                                              dark
+                                              v-bind="attrs"
+                                              v-on="on"
+                                            >
+                                             <v-icon md="1"
+                                                color="#D71700"
+                                             > {{icons.mdiPlus}}
+
+                                             </v-icon>
+                                            </v-btn>
+                                          </template>
+
+                                          <v-card>
+                                            <v-card-title class="headline grey lighten-2" >
+                                              Add #code for this task
+                                            </v-card-title>
+
+                                            <v-card-text>
+                                              <v-text-field
+                                                v-model="newHashCode"
+                                                @keypress.enter = "addNewHashCode()"
+                                                placeholder="#"></v-text-field>
+                                            </v-card-text>
+
+                                            <v-divider></v-divider>
+
+                                            <v-card-actions>
+                                              <v-spacer></v-spacer>
+                                              <v-btn
+                                                color="#D71700"
+                                                text
+                                                @click = "addNewHashCode()"
+
+                                              >
+                                                Add
+                                              </v-btn>
+                                            </v-card-actions>
+                                          </v-card>
+                                        </v-dialog>
+                                      </div>
+                                      </template>
+                                       <span>Add hash-code to task for quick access</span>
+                     </v-tooltip>
+
+                </v-col>
+                <v-col
+                  cols="3"
+                  md="2"
+                  :offset="defaultSelected.hash == '#' ? 0 : 1"
+                  :offset-md="defaultSelected.hash == '#' ? 0 : 1"
+                >
+                    <v-select
+                        :items="defaultSelected.hashCodes"
+                        value="defaultSelected.hashCodes[0]"
+                        v-model="defaultSelected.hash"
+                        @change="onChange"
+                        required>
+
+                    </v-select>
+                </v-col>
+                <v-col cols="4" md="3">
+                    <v-text-field
+                        :placeholder=" placeholders[0] "
+                        :counter="25"
+                        required
+                        v-model="defaultSelected.taskName"
+                        @input="inputChangeHandler"
+                        @keypress.enter = "addTask"
+                    ></v-text-field>
+                    </v-col>
+                    <v-col cols="1" md="3">
+                    <v-select
+                           :label="placeholders[1]"
+                           required
+                           :items="taskTypes"
+                           v-model="defaultSelected.type"
+                     ></v-select>
+                    </v-col>
+                    <v-col cols="1" md="1">
+                        <v-select
+                                           :label="placeholders[2]"
+                                           required
+                                           :items="taskPriority"
+                                           v-model="defaultSelected.priority"
+
+                        ></v-select>
+                    </v-col>
+                    <v-col cols="1" md="1">
+                        <v-text-field
+                              v-model="defaultSelected.time"
+                              label="Time for task"
+                            >00:00</v-text-field>
+                    </v-col>
+
+              </v-row>
+              <v-row>
+                <v-col cols="5" md="5" offset="1" offset-md="1">
+                     <v-textarea
+
+                                         outlined
+                                         rows="2"
+                                         row-height="4"
+                                         v-model="defaultSelected.details"
+                                         shaped
+                               :placeholder="placeholders[4]"
+                               :counter="256"
+                     ></v-textarea>
+                 </v-col>
+                 <v-col cols="5" md="5">
+                    <v-text-field
+                                    :placeholder=" placeholders[5] "
+                                    v-model="defaultSelected.notes"
+                                    @keypress.enter = "addTask"
+                    ></v-text-field>
+                 </v-col>
+                <v-col cols="1" md="1">
+                   <v-tooltip right>
+                     <template v-slot:activator="{ on }">
+                        <div v-on="on">
+                            <v-btn icon>
+                                       <v-icon md="1"
+                                            color="#D71700"
+                                            v-on:click="addTask"> {{icons.mdiPlex}}
+                                        </v-icon>
+                            </v-btn>
+                        </div>
                         </template>
-                </v-data-table>
-                
-                </div>
-
-            </div>
-
-              <div v-if="items.length > 0">
-                    <v-row align="center" class="d-flex justify-start mb-6">
-                         <v-btn color="#D71700" style="text-color:#ffffff" icon v-on:click="formSubmit()">
-                           <v-icon md="1"
-                                   color="#D71700"
-                           >
-                            {{icons.mdiStarThreePointsOutline}}
-                           </v-icon>
-
-                         </v-btn>
-                         </v-row >
-                         <v-alert
-                          :value="showAlert"
-                          :type="alertType"
-                        >{{serverMessage}}</v-alert>
+                        <span>Add task to day plan</span>
+                    </v-tooltip>
+                </v-col>
+              </v-row>
               </div>
-            </tr>
-        </table>
-      </div>
-    </v-container>
+              <v-divider></v-divider>
+                            <v-data-table
+                                :headers="headers"
+                                :items="items"
+                                class="elevation-1"
+                                @dblclick:tr="deleteItem"
+                              >
+                                <template v-slot:item="props">
+                                    <tr align="center"  ref="refWord">
+                                        <td>{{ props.item.hash }}</td>
+                                        <td>{{ props.item.taskName }}</td>
+                                        <td>{{ props.item.type }}</td>
+                                        <td>{{ props.item.priority }}</td>
+                                        <td>{{ props.item.time }}</td>
+                                        <td>{{ props.item.details }}</td>
+                                        <td>{{ props.item.notes }}</td>
+                                        <td><v-btn @click="deleteItem(props.item)">Delete task</v-btn></td>
+                                    </tr>
+                                </template>
+                        </v-data-table>
+                        
+                      <div v-if="items.length > 0">
+    <div class=" d-flex text-left">
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="#D71700" style="text-color:#ffffff" icon v-on:click="formSubmit()" v-bind="attrs"
+                                        v-on="on">
+                                       <v-icon md="1"
+                                               color="#D71700"
+                                       >
+                                        {{icons.mdiStarThreePointsOutline}}
+                                       </v-icon>
 
+                                     </v-btn>
+        </template>
+        <span>Submit</span>
+      </v-tooltip>
+  
+    
+    </div>
+
+                      </div>
+                                 <v-alert
+                                  color="#404040"
+                                  text
+                                  class="elevation-1"
+                                  :value="showAlert"
+                                  :type="alertType"
+                                >{{serverMessage}}</v-alert>
+        </v-container>
+    </v-card>
 </template>
 <script>
 import Vuetify from 'vuetify/lib'
-
 
 import {
     mdiAccount,
@@ -225,8 +224,6 @@ import {
     mdiAlarm,
     mdiPlus
   } from '@mdi/js'
-
-
 
   export default {
     data: () => ({
@@ -329,7 +326,6 @@ import {
         },
 
          onChange(event) {
-
              let currentObj = this;
              axios.post('/getSavedTask', {hash_code: event})
                   .then(function (response) {
@@ -347,12 +343,7 @@ import {
          },
 
         addTask(e) {
-            /*мне нужно каждому полю из моего стартового меню присвоить модель и запихнуть значение в taskName
-            * тогда я получу json-объект для отправки на серсер
-            */
             this.items.push(this.defaultSelected);
-            /*check*/
-            /*end*/
             this.showIcon   = 0;
             this.defaultSelected = {
                     hashCodes : this.defaultSelected.hashCodes,
@@ -427,13 +418,7 @@ import {
               .catch(function (error) {
                    currentObj.output = error;
               });
-    },
-    watch:{
-        taskName: function (){
-             //++this.showIcon;
-        }
     }
-
 
   }
 </script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,9 @@
 
 @section('content')
 <div class="container">
-<v-app>
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header" style="background-color: #A10000; color:#FFFFFF">Create your day plan!</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <plan-component>
-                    </plan-component>
-                </div>
-            </div>
-        </div>
-    </div>
-</v-app>
+    <v-app>
+        <plan-component>
+        </plan-component>       
+    </v-app>
 </div>
 @endsection
