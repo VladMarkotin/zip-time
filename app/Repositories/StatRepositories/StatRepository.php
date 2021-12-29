e107158e94be41686ac3eebcc0527b316c6d2466
Vlad <vlad.markotin@gmail.com>
2021-12-29 14:20:29 +0300

    Optimization of statistic

--- app/Repositories/StatRepositories/StatRepository.php
+++ app/Repositories/StatRepositories/StatRepository.php
@@ -30,14 +30,15 @@ class StatRepository
     private function getStatForPeriod($period)
     {
         /*Get stat for the period. Stat means: AVG mark, AVG personal mark, Max Mark, Min Mark, Median Mark*/
-        $response['from']       = $period['from'];
-        $response['to']       = $period['to'];
-        $response['avgMark']    = AverageMarkTrait::getAvgMark($period);
-        $response['ownAvgMark'] = AverageMarkTrait::getOwnAvgMark($period);
-        $response['medianMark'] = AverageMarkTrait::getMedianValue($period);
-        $response['maxMark']    = AverageMarkTrait::getMaxMark($period);
-        $response['minMark']    = AverageMarkTrait::getMinMark($period);
+        $response['from']          = $period['from'];
+        $response['to']            = $period['to'];
+        $response['avgMark']       = AverageMarkTrait::getAvgMark($period, 1);
+        $response['ownAvgMark']    = AverageMarkTrait::getAvgMark($period, 2);
+        $response['medianMark']    = AverageMarkTrait::getMedianValue($period);
+        $response['medianOwnMark'] = AverageMarkTrait::getMedianValue($period, 2);
+        $response['maxMark']       = AverageMarkTrait::getExtremum($period);
+        $response['minMark']       = AverageMarkTrait::getExtremum($period, 2);
 
         return $response;
     }
-} 
+}
--- app/Repositories/StatRepositories/traits/AverageMarkTrait.php
+++ app/Repositories/StatRepositories/traits/AverageMarkTrait.php
@@ -15,44 +15,44 @@ use MathPHP\Statistics\Average;
 trait AverageMarkTrait
 {
     /*Получает среднюю оценку*/
-    function getAvgMark($period)
+    function getAvgMark($period, $type = 1)
     {
         $data['id'] = Auth::id();
-        $query = $query = "SELECT AVG(final_estimation) avgMark FROM `timetables` WHERE final_estimation <> 0.00
+        if($type == 1){
+            $query = $query = "SELECT AVG(final_estimation) avgMark FROM `timetables` WHERE final_estimation <> 0.00
                             AND (day_status = 3
                              OR  day_status = -1)
                             AND user_id = $data[id] ";
-        if(is_array($period)){
-            $query .= " AND date BETWEEN '$period[from]' AND '$period[to]' ";
         } else{
-            $query .= "AND date = '$period' ";
-        }
-        $response = DB::select($query);
-
-        return $response[0]->avgMark;
-    }
-
-    /*Получает среднюю личную оценку*/
-    function getOwnAvgMark($period)
-    {
-        $data['id'] = Auth::id();
-        $query = $query = "SELECT AVG(own_estimation) ownMark FROM `timetables` WHERE day_status = 3
+            $query = "SELECT AVG(own_estimation) ownMark FROM `timetables` WHERE day_status = 3
                               AND own_estimation <> 0.00
                               AND user_id = $data[id] ";
+        }
+
         if(is_array($period)){
             $query .= " AND date BETWEEN '$period[from]' AND '$period[to]' ";
         } else{
             $query .= "AND date = '$period' ";
         }
         $response = DB::select($query);
+        if($type == 1){
+            $mark = $response[0]->avgMark;
+        } else{
+            $mark = $response[0]->ownMark;
+        }
 
-        return $response[0]->ownMark;
+        return $mark;
     }
 
-    function getMedianValue($period)
+    function getMedianValue($period, $type = 1) //type=1-final_estimation, type=2-own_estimation
     {
         $data['id'] = Auth::id();
-        $query = $query = "SELECT final_estimation fE FROM `timetables` WHERE final_estimation <> 0.00 AND user_id = $data[id] ";
+        if($type == 1){
+            $query = "SELECT final_estimation fE FROM `timetables` WHERE final_estimation <> 0.00
+                        AND user_id = $data[id] ";
+        } else{
+            $query = "SELECT own_estimation oE FROM `timetables` WHERE own_estimation <> 0.00 AND user_id = $data[id] ";
+        }
         if(is_array($period)){
             $query .= " AND date BETWEEN '$period[from]' AND '$period[to]' ";
         } else{
@@ -60,40 +60,32 @@ trait AverageMarkTrait
         }
         $response = DB::select($query);
         foreach ($response as $v){
-            $medianValues[] = $v->fE;
+            ($type == 1) ? $medianValues[] = $v->fE
+                          : $medianValues[] = $v->oE;
         }
         $median = Average::median($medianValues);
 
         return $median;
     }
 
-    function getMaxMark($period)
+    function getExtremum($period, $type = 1)
     {
         $data['id'] = Auth::id();
-        $query = "SELECT MAX(final_estimation) maxMark FROM `timetables` WHERE user_id = $data[id] ";
+        if($type == 1){
+            $query = "SELECT MAX(final_estimation) maxMark FROM `timetables` WHERE user_id = $data[id] ";
+        } else{
+            $query = "SELECT MIN(final_estimation) minMark FROM `timetables` WHERE user_id = $data[id]
+                                                                               AND day_status = 3";
+        }
+
         if(is_array($period)){
             $query .= " AND date BETWEEN '$period[from]' AND '$period[to]' ";
         } else{
             $query .= "AND date = '$period' ";
         }
         $response = DB::select($query);
+        ($type == 1) ? $response = $response[0]->maxMark : $response = $response[0]->minMark;
 
-        return $response[0]->maxMark;
+        return $response;
     }
-
-    function getMinMark($period)
-    {
-        $data['id'] = Auth::id();
-        $query = "SELECT MIN(final_estimation) minMark FROM `timetables` WHERE user_id = $data[id] ";
-        if(is_array($period)){
-            $query .= " AND date BETWEEN '$period[from]' AND '$period[to]' ";
-        } else{
-            $query .= "AND date = '$period' ";
-        }
-        $response = DB::select($query);
-
-        return $response[0]->minMark;
-    }
-
-
-} 
+}
