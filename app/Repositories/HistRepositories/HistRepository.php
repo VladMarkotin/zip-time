2e92ec5d2b30e301bb2cb9881ad1be39708847b0
Vladislav <vlad.markotin@gmail.com>
2022-01-08 18:41:02 +0300

    Add history logic

--- app/Repositories/HistRepositories/HistRepository.php
+++ app/Repositories/HistRepositories/HistRepository.php
@@ -9,6 +9,7 @@ namespace App\Repositories\HistRepositories;
 
 
 use Illuminate\Support\Facades\DB;
+use App\Repositories\HistRepositories\traits\TransformHistTrait;
 
 class HistRepository
 {
@@ -25,10 +26,13 @@ class HistRepository
     private function getDataFromDB()
     {
         $_this = $this;
+        $addClosedDays = function () {
+            return " AND timetables.day_status = 3 ";
+        };
         $addWeekends = function () use ($_this){
             $query = "";
             if($this->period['with_weekend']){
-                $query = " AND timetables.day_status = 1 ";
+                $query = " OR timetables.day_status = 1 ";
             }
 
             return $query;
@@ -36,20 +40,21 @@ class HistRepository
         $addWithFailed = function () use ($_this){
             $query = "";
             if($this->period['with_weekend']){
-                $query = " AND timetables.day_status = -1 ";
+                $query = " OR timetables.day_status = -1 ";
             }
 
             return $query;
         };
+        $closedDays = $addClosedDays();
         $weekend    = $addWeekends();
         $withFailed = $addWithFailed();
-        $query = "SELECT  timetables.date, timetables.day_status, timetables.final_estimation, timetables.own_estimation,
-                    timetables.comment, tasks.hash_code, tasks.task_name, tasks.type, tasks.priority, tasks.details,
+        $query = "SELECT  timetables.id, timetables.user_id, timetables.date, timetables.day_status, timetables.final_estimation, timetables.own_estimation,
+                    timetables.comment, tasks.id, tasks.hash_code, tasks.task_name, tasks.type, tasks.priority, tasks.details,
                      tasks.time, tasks.mark, tasks.note FROM timetables JOIN tasks ON timetables.id = tasks.timetable_id
-                      WHERE timetables.date BETWEEN '".$this->period['from'] ."' AND '".$this->period['to']."' $weekend $withFailed";
-        dd($query);
+                      WHERE timetables.date BETWEEN '".$this->period['from'] ."' AND '".$this->period['to']."'$closedDays $weekend $withFailed";
         $response = DB::select($query);
+        $history = TransformHistTrait::transformData($response, $this->period);
 
-        return $response;
+        return $history;
     }
 } 
---
+++ app/Repositories/HistRepositories/traits/TransformHistTrait.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vlad
+ * Date: 05.01.2022
+ * Time: 8:58
+ */
+namespace App\Repositories\HistRepositories\traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait TransformHistTrait
+{
+    function transformData(array $data, array $period)
+    {
+        $finalArray = [
+            "userId" => Auth::id(),
+            "from"   => $period['from'],
+            "to"     => $period['to'],
+            "plans"  => [],
+        ];
+        /*
+         * I have to get this structure:
+         * [
+         *     [
+         *      "userId"            =>
+         *      "from"              =>
+         *      "to"                =>
+         *      "plans"             => [
+         *                                "<date>" => [
+                 *                                      "timetable_id"     =>
+                 *                                      "day_status"       =>
+                 *                                      "final_estimation" =>
+                 *                                      "own_estimation"   =>
+                 *                                      "comment"          =>
+                 *                                      "tasks" => [
+                 *                                        [
+                 *                                            "id"               =>
+                     *                                        "task_name"        =>
+                     *                                        "hash_code"        =>
+                     *                                        "type"             =>
+                     *                                        "priority"         =>
+                     *                                        "details"          =>
+                     *                                        "time"             =>
+                     *                                        "mark"             =>
+                     *                                        "note"             =>
+                     *                                        "created_at"       =>
+                     *                                        "updated_at"       =>
+             *                                             ],
+             *                                             [
+                     *                                        "id"               =>
+             *                                                "task_name"        =>
+                     *                                        "hash_code"        =>
+                     *                                        "type"             =>
+                     *                                        "priority"         =>
+                     *                                        "details"          =>
+                     *                                        "time"             =>
+                     *                                        "mark"             =>
+                     *                                        "note"             =>
+                     *                                        "created_at"       =>
+                     *                                        "updated_at"       =>
+         *                                                ],
+         *                                              ],
+         *                                             ],
+         *     ]
+        */
+        $finalArray = self::createPlanArray($data, $finalArray);
+
+        return $finalArray;
+    }
+
+    function createPlanArray(array $data, array $finalArray)
+    {
+        $i = 0;
+        foreach($data as $k => $v){
+            $finalArray["plans"][$v->date]['timetableId']                   = $v->id;
+            $finalArray["plans"][$v->date]['dayStatus']                     = $v->day_status;
+            $finalArray["plans"][$v->date]['finalEstimation']               = $v->final_estimation;
+            $finalArray["plans"][$v->date]['ownEstimation']                 = $v->own_estimation;
+            $finalArray["plans"][$v->date]['comment']                       = $v->comment;
+            $finalArray["plans"][$v->date]['tasks'][$i]["hashCode"]         = $v->hash_code;
+            $finalArray["plans"][$v->date]['tasks'][$i]["taskName"]         = $v->task_name;
+            $finalArray["plans"][$v->date]['tasks'][$i]["type"]             = $v->type;
+            $finalArray["plans"][$v->date]['tasks'][$i]["priority"]         = $v->priority;
+            $finalArray["plans"][$v->date]['tasks'][$i]["details"]          = $v->details;
+            $finalArray["plans"][$v->date]['tasks'][$i]["time"]             = $v->time;
+            $finalArray["plans"][$v->date]['tasks'][$i]["mark"]             = $v->mark;
+            $finalArray["plans"][$v->date]['tasks'][$i]["note"]             = $v->note;
+            $i++;
+        }
+
+        return $finalArray;
+    }
+} 
