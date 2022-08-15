383ca3c33e96dc30bb667852a775a1dfb2b42e2c
3051285A035PB0 <3051285A035PB0@gmail.com>
2022-08-15 23:08:27 +0300

    Raw history was added.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,10 @@
 @section('content')
 <div class="container">
 	<v-app>
-		<button style="text-decoration: underline;" v-on:click="currComponent.name = 'Stat'">Statistics</button>
+		<div>
+			<button style="text-decoration: underline;" v-on:click="currComponent.name = 'Stat'">Statistics</button>
+			<button style="text-decoration: underline;" v-on:click="currComponent.name = 'History'">History</button>
+		</div>
 		<component v-bind:is="currComponentName" v-bind:data="currComponentProps">
 	</v-app>
 </div>
