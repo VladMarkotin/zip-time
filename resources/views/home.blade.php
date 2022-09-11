6a4b8480e1f88439be4bf9c67b09ad0ebd10dc79
vladislav markotin <vlad.markotin@gmail.com>
2022-09-11 20:32:27 +0300

    Backlog button added everywere

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 	@if (Auth::check())
 	 <a class = "backlog-add-button" data-bs-toggle="modal" data-bs-target="#AddbacklogModal">
-        <i type="submit" class="fa fa-plus-square" style="color: #A10000; font-size: 35px"></i>
+        <i type="submit" class="fa fa-plus-square" style="color: #A10000; font-size: 35px; margin-left: 155px;"></i>
     </a>	
 	@endif 
     <div class="container">
