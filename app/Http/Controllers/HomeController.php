d892a21747a487e342d7daf5b44fd92e53ce5b07
Francis <shevy2k2@yahoo.com>
2022-10-11 20:10:40 +0300

    rating completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,8 +37,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-       $this->estimateDayRepository->estimate();
-      
         return view('home');
     }
 }
