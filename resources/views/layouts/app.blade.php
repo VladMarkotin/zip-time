82ac3889abff6f682c65c4428b712c6f4712f30f
Francis <shevy2k2@yahoo.com>
2022-12-03 16:09:42 +0300

    notification update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,19 +42,25 @@
                     @auth
 
                         <!-- Button Notification -->
-
-                        <div class="btn-group">
+ 
+                        <div class="btn-group" id="notification_section">
                             <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
-                                Уведомления
+                             уведомления
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-bell" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
+                                    <path  @if ($notifications)
+                                    style=" animation: bell 4s linear infinite; "
+                                    @endif 
+                                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
                                 </svg>
-                                <span class="badge badge-pill " style="background: #a10000; color:white"> <span
-                                        id="main_notification_button">{{ $count_notifications }}</span></span>
+                                <span class="badge badge-pill notification--num" @if ($notifications)
+                                style="background: #a10000; color:white;  animation: notification 2.2s linear infinite;"
+                                @endif > 
+ 
+                                 <span id="main_notification_button">{{ $count_notifications }}</span></span>
 
+                    
                             </button>
                             <div class="dropdown-menu notifications">
                                 @foreach ($notifications as $notification)
@@ -63,6 +69,10 @@
                             </div>
                             <input type="hidden" name="_token" id="_token" value={{ csrf_token() }}>
                         </div>
+
+
+                 
+                    
 
                         <!-- Button trigger modal -->
                         <button type="button" class="btn btn-light" data-toggle="modal" data-target="#addNotification">
@@ -74,6 +84,7 @@
                                 Отправить через Pusher
                             </button>
                         @endif
+
 
                         <!-- Modal Создать уведомление-->
                         <div class="modal fade" id="addNotification" tabindex="-1" role="dialog"
@@ -286,6 +297,7 @@
     <script>
         window.addEventListener("load", function() {
             $('body').on('click', '#saveNotification', function() {
+
                 let token = $('#_token').val();
                 let type = $('#typeNotification').val();
                 let data = $('#dataNotification').val();
@@ -298,6 +310,8 @@
                     notification_date: notification_date
                 }, function() {}).done(function(response) {
                     $('#addNotification').modal('hide');
+                    $("#notification_section").load(" #notification_section");
+                    
                 })
             })
         })
