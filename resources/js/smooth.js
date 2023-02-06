db080cebc210a3433559382203acb193bda77c37
Markov Andrey <andremarkov@icloud.com>
2023-02-07 00:25:34 +0300

    code smooth refactoring

--- resources/js/smooth.js
+++ resources/js/smooth.js
@@ -1,95 +1,74 @@
 /*Smooth*/
 let pagesMax = 0;
-
-$(document).ready(function(){
-
-
-    // Create navigation list
-    $("body").append("<ul class='smooth-navigation-menu'></li></ul>");
-
-
-    // Define section classes
-    $(".smooth-section").each(function(index){
-        $(this).addClass("smooth-section-"+index);
-        pagesMax++;
-
-        // Add section on navigation list
-        $(".smooth-navigation-menu").append("<li class='smooth-navigation-item' data-section='"+index+"'></li>");
-
-    });
-
-
-    // Item navigation list event
-    $(".smooth-navigation-item").click(function(){
-        let section = $(this).attr("data-section");
-        $("html, body").stop().animate({scrollTop: $(".smooth-section-"+section).offset().top},1000);
-        pageIndex = section;
-    });
-
-});
-
-
-// Control scroll time
 let time = new Date().getTime();
-
-// Scroll events (mouse and keys)
-$(window).bind("keydown.key40 DOMMouseScroll mousewheel",function(event){
-
-    // Update time
-    let now = new Date().getTime();
-
-    // Check page refresh. (F5)
-    if(event.keyCode == 116){
-        location.reload();
-    }
-
-
-    // Check time of scrolling...
-    if (now - time >= 100) {
-        time = now;
-        // Down event
-        if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 || event.keyCode == 40) {
-            // Next section element
-            $(".smooth-section").nextPage();
-        } else if (event.originalEvent.detail < 0 || event.originalEvent.wheelDelta > 0 || event.keyCode == 38) {
-            // Previous section element
-            $(".smooth-section").prevPage();
-        }
-
-        event.stopPropagation();
-    } else {
-        time = now;
-        return 0;
-    }
-
-});
-
-
-// Section index number/position
-pageIndex = 0;
+let limit = 250;
+let pageIndex = localStorage.getItem("pageIndex");
+localStorage.removeItem("pageIndex");
 
 // Next section function
 $.fn.nextPage = function () {
-    if(pageIndex < pagesMax-1){
-
-        pageIndex++;
-
-        let top = $(this).next(".smooth-section-"+pageIndex).offset().top;
-        $("html, body").stop().animate({scrollTop: top},1000);
-
-    }
-
+  if(pageIndex < pagesMax-1){
+      pageIndex++;
+      let top = $(this).next(".smooth-section-"+pageIndex).offset().top;
+      $("html, body").stop().animate({scrollTop: top},1000);
+  }
 };
 
 // Previous section function
 $.fn.prevPage = function () {
-    if(pageIndex > 0){
-
-        pageIndex--;
-
-        let top = $(this).prev(".smooth-section-"+pageIndex).offset().top;
-        $("html, body").stop().animate({scrollTop: top},1000);
-
-    }
-
+  if(pageIndex > 0){
+      pageIndex--;
+      let top = $(this).prev(".smooth-section-"+pageIndex).offset().top;
+      $("html, body").stop().animate({scrollTop: top},1000);
+  }
 };
+
+//Document ready
+$(document).ready(function(){
+  // Create navigation list
+  $("body").append("<ul class='smooth-navigation-menu'></li></ul>");
+  // Define section classes
+  $(".smooth-section").each(function(index){
+      $(this).addClass("smooth-section-"+index);
+      pagesMax++;
+      // Add section on navigation list
+      $(".smooth-navigation-menu").append("<li class='smooth-navigation-item' data-section='"+index+"'></li>");
+  });
+  // Item navigation list event
+  $(".smooth-navigation-item").click(function(){
+      let section = $(this).attr("data-section");
+      $("html, body").stop().animate({scrollTop: $(".smooth-section-"+section).offset().top},1000);
+      pageIndex = section;
+  });
+
+  let throttle = (func, limit) => {
+    let inThrottle;
+    return function () {
+      const args = arguments;
+      const context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+      }
+    };
+  };
+
+  $(window).bind("keydown.key40 DOMMouseScroll mousewheel",
+    throttle(function (event) {
+      let now = new Date().getTime();
+      if (now - time >= 100) {
+        time = now;
+        if (event.keyCode === 116) {
+          localStorage.setItem("pageIndex", pageIndex);
+          location.reload();
+        } else if (event.keyCode == 40 || event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) {
+          $(".smooth-section").nextPage();
+        } else if (event.keyCode == 38 || event.originalEvent.detail < 0 || event.originalEvent.wheelDelta > 0) {
+          $(".smooth-section").prevPage();
+        }
+        event.stopPropagation();
+      }
+    }, limit)
+  );
+});
