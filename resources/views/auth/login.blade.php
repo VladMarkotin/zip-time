c72624220208b832dcec434bdb2c04380a83fd9c
XenoProxy <xenoproxy@yandex.by>
2023-10-20 17:52:52 +0300

    style for auth form and footer fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-            <Facebook/>
-                
-            </div>
+            <div class="auth-container">
+                <div class="auth-info">                    
+                    <div class="auth-text">
+                        To continue using Quipl.io you have to Log in with your Facebook account
+                    </div>
+                </div>
+                <div class="card auth-card">
+                    <img class="facebook-ico" src="/images/facebook.png">
+                    <div class="facebook-link">
+                        <Facebook/>
+                    </div>                                    
+                </div>
+            </div>            
         </div>
     </div>
 </div>
