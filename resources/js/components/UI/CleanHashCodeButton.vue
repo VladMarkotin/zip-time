d18d9ced86162f96aea366dd39592d87e20cf23f
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2023-11-02 15:49:27 +0300

    add CleanHashCodeButton component to git index

---
+++ resources/js/components/UI/CleanHashCodeButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-tooltip right>
+		<template v-slot:activator="{on}">
+			<v-btn icon v-on="on" v-on:click="$emit('clearCurrentHashCode')">
+				<v-icon color="#D71700">{{icon.mdiBackspace}}</v-icon>
+			</v-btn>
+		</template>
+		<span>{{ tooltipText }}</span>
+	</v-tooltip>
+</template>
+
+<script>
+import {mdiBackspace} from '@mdi/js';
+    export default {
+        props: {
+            tooltipText: {
+                default: 'Clear current hash code',
+            }
+        },
+        data() {
+            return {
+                icon : {mdiBackspace},
+            }
+        }
+    }
+</script>
