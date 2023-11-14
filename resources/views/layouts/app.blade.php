99f05030ba9354872d195cbd1676ce8776810ed1
Vlad <vlad.markotin@gmail.com>
2023-11-14 21:53:49 +0300

    new index design (unfinished)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+
     @livewireStyles
 
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,38 @@ const mix = require('laravel-mix');
 
 mix
     .js('resources/js/app.js', 'public/js')
+    .js('resources/js/design/main.js', 'public/js')
+    .js('resources/js/design/plugins.js', 'public/js')
+    .js('resources/js/design/owl.carousel.min.js', 'public/js')
+    .js('resources/js/design/masonry.pkgd.js', 'public/js')
+    .js('resources/js/design/masonry.pkgd.min.js', 'public/js')
+
+    .js('resources/js/design/waypoints.min.js', 'public/js')
+
+    .js('resources/js/design/ajax-mail.js', 'public/js')
+    .js('resources/js/design/jquery.counterup.min.js', 'public/js')
+    .js('resources/js/design/jquery.magnific-popup.min.js', 'public/js')
+    .js('resources/js/design/jquery.mb.YTPlayer.min.js', 'public/js')
+
+    .js('resources/js/design/jquery.meanmenu.js', 'public/js')
+    .js('resources/js/design/jquery.mixitup.min.js', 'public/js')
+    .js('resources/js/design/jquery.nav.js', 'public/js')
+    .js('resources/js/design/jquery.parallax-1.1.3.js', 'public/js')
+
+    .js('resources/js/design/vendor/jquery-1.12.0.min', 'public/js')
+    .js('resources/js/design/vendor/modernizr-2.8.3.min', 'public/js')
+
+    
     .vue()
-    .css('resources/css/app.css', 'public/css/app.css')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/customTooltip/customTooltip.scss', 'public/css');
+
+    mix.styles([
+        'resources/css/app.css',
+        'resources/css/animate.css',
+        'resources/css/animate-text.css',
+        'resources/css/bootstrap.min.css',
+        'resources/css/et-line.css',
+
+    ], 'public/css/all.css')
+
+    .sass('resources/sass/app.scss', 'public/css/style.css')
+    .sass('resources/sass/customTooltip/customTooltip.scss', 'public/css/style.css'); 
