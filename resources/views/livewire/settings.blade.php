ecd065aa9542488afad07ae8e96c0842e9eee497
vladislav markotin <vlad.markotin@gmail.com>
2023-12-12 19:14:20 +0300

    settings2 branch

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -9,10 +9,6 @@
            aria-controls="v-pills-home" aria-selected="true" wire:ignore>
            Saved tasks
     </a>
-    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" 
-        role="tab" aria-controls="v-pills-profile" aria-selected="false"wire:ignore>
-        Statistic
-    </a>
     <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
      aria-controls="v-pills-messages" aria-selected="false" wire:ignore>
         Personal settings
