e0a757506247db85d4a3a24c72c62c613c43e754
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2023-12-23 16:18:02 +0300

    update sortable status in fields: details, notes

--- resources/js/components/cards/Plan/Plan.vue
+++ resources/js/components/cards/Plan/Plan.vue
@@ -129,43 +129,6 @@
          :items          = "items"
          @deleteItem  = "deleteItem"
          />
-         <!-- <v-data-table
-            :headers="headers"
-            :items="items"
-            class="elevation-1"
-            id="plan-tasks-table"
-            >
-            <template v-slot:item="props">
-               <tr 
-               align="center"  
-               ref="refWord"
-               @dblclick="deleteItem(props.item)"
-               >
-                  <td>{{ props.item.hash }}</td>
-                  <td>{{ props.item.taskName }}</td>
-                  <td>{{ props.item.type }}</td>
-                  <td>{{ props.item.priority }}</td>
-                  <td>{{ props.item.time }}</td>
-                  <td>{{ props.item.details }}</td>
-                  <td>{{ props.item.notes }}</td>
-                  <td>
-                     <v-tooltip right>
-                        <template v-slot:activator="{ on, attrs }">
-                           <v-btn color="#D71700" style="text-color:#ffffff" icon v-on:click="deleteItem(props.item)" v-bind="attrs"
-                              v-on="on">
-                              <v-icon md="1"
-                                 color="#D71700"
-                                 >
-                                 {{icons.mdiDelete}}
-                              </v-icon>
-                           </v-btn>
-                        </template>
-                        <span>Delete task</span>
-                     </v-tooltip>
-                  </td>
-               </tr>
-            </template>
-         </v-data-table> -->
          <v-row>
          <v-col>
             <div 
@@ -249,7 +212,6 @@ import {
     mdiPlex,
     mdiPencil,
     mdiShareVariant,
-   //  mdiDelete,
     mdiStarThreePointsOutline,
     mdiClockStart,
     mdiAlarm,
@@ -281,71 +243,10 @@ export default {
             notes: ''
         },
         preparedTask: {},
-      //   headers: [{
-      //           text: '#code',
-      //           value: 'hash',
-      //           align: 'center'
-      //       },
-      //       {
-      //           text: 'Task name',
-      //           align: 'center',
-      //           value: 'taskName',
-      //           groupable: false,
-      //       },
-      //       {
-      //           text: 'Task Type',
-      //           value: 'type',
-      //           align: 'center',
-      //           sort: (taskTypeOne, taskTypeTwo) => {
-
-      //              const getPrior = (taskType) => {
-      //                const typePrior = {
-      //                   'required job':     4,
-      //                   'non required job': 3,
-      //                   'required task':    2,
-      //                   'task':             1,
-      //                }
-
-      //                return typePrior[taskType];
-      //             }
-
-      //             const [taskOnePrior, taskTwoPrior] = ([taskTypeOne, taskTypeTwo].map(item => getPrior(item)));
-                  
-      //             return taskOnePrior - taskTwoPrior;
-      //           },
-      //       },
-      //       {
-      //           text: 'Task Priority',
-      //           value: 'priority',
-      //           align: 'center'
-      //       },
-      //       {
-      //           text: 'Time',
-      //           value: 'time',
-      //           align: 'center'
-      //       },
-      //       {
-      //           text: 'Details',
-      //           value: 'details',
-      //           align: 'center',
-      //       },
-      //       {
-      //           text: 'Notes',
-      //           value: 'notes',
-      //           align: 'center',
-      //       },
-      //       {
-      //           text: 'Action',
-      //           value: '',
-      //           align: 'center',
-      //           sortable: false,
-      //       },
-      //   ],
         items: [],
         icons: {
             mdiAccount,
             mdiPlex,
-            // mdiDelete,
             mdiPencil,
             mdiStarThreePointsOutline,
             mdiClockStart,
@@ -494,7 +395,6 @@ export default {
             }
         },
         deleteItem(item) {
-            console.log('+');
             var index = this.items.indexOf(item)
             this.items.splice(index, 1);
         },
--- resources/js/components/cards/Plan/PreplanTasksTable.vue
+++ resources/js/components/cards/Plan/PreplanTasksTable.vue
@@ -1,41 +1,39 @@
 <template>
-    <div>
-        <v-data-table 
-        :headers="headers" 
-        :items="items" 
-        class="elevation-1" 
-        id="plan-tasks-table">
-            <template v-slot:item="props">
-                <tr align="center" ref="refWord" @dblclick="deleteItem(props.item)">
-                    <td>{{ props.item.hash }}</td>
-                    <td>{{ props.item.taskName }}</td>
-                    <td>{{ props.item.type }}</td>
-                    <td>{{ props.item.priority }}</td>
-                    <td>{{ props.item.time }}</td>
-                    <td>{{ props.item.details }}</td>
-                    <td>{{ props.item.notes }}</td>
-                    <td>
-                        <v-tooltip right>
-                            <template v-slot:activator="{ on, attrs }">
-                                <v-btn 
-                                v-bind="attrs" v-on="on"
-                                v-on:click="deleteItem(props.item)"
-                                color="#D71700" 
-                                style="text-color:#ffffff" 
-                                icon 
-                                >
-                                    <v-icon md="1" color="#D71700">
-                                        {{ icons.mdiDelete }}
-                                    </v-icon>
-                                </v-btn>
-                            </template>
-                            <span>Delete task</span>
-                        </v-tooltip>
-                    </td>
-                </tr>
-            </template>
-        </v-data-table>
-    </div>
+    <v-data-table 
+    :headers="headers" 
+    :items="items" 
+    class="elevation-1" 
+    id="plan-tasks-table">
+        <template v-slot:item="props">
+            <tr align="center" ref="refWord" @dblclick="deleteItem(props.item)">
+                <td>{{ props.item.hash }}</td>
+                <td>{{ props.item.taskName }}</td>
+                <td>{{ props.item.type }}</td>
+                <td>{{ props.item.priority }}</td>
+                <td>{{ props.item.time }}</td>
+                <td>{{ props.item.details }}</td>
+                <td>{{ props.item.notes }}</td>
+                <td>
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn 
+                            v-bind="attrs" v-on="on"
+                            v-on:click="deleteItem(props.item)"
+                            color="#D71700" 
+                            style="text-color:#ffffff" 
+                            icon 
+                            >
+                                <v-icon md="1" color="#D71700">
+                                    {{ icons.mdiDelete }}
+                                </v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Delete task</span>
+                    </v-tooltip>
+                </td>
+            </tr>
+        </template>
+    </v-data-table>
 </template>
 
 <script>
@@ -98,11 +96,13 @@ export default {
                 text: 'Details',
                 value: 'details',
                 align: 'center',
+                sortable: false,
             },
             {
                 text: 'Notes',
                 value: 'notes',
                 align: 'center',
+                sortable: false,
             },
             {
                 text: 'Action',
