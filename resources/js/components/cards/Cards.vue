b58641838cc17f9c2facbabae538f6d3e3a72158
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-01-08 01:38:31 +0300

    update

--- resources/js/components/cards/Cards.vue
+++ resources/js/components/cards/Cards.vue
@@ -3,12 +3,28 @@
         <div>{{title}}</div>
         <v-row>
             <v-col cols="6" v-for="item, i of items" v-bind:key="item.taskId">
-                <Card v-bind:item="item" v-bind:num="i" />
+                <Card 
+                v-bind:item="item" 
+                v-bind:num="i" 
+                @updateItem="updateItems"
+                />
             </v-col>
         </v-row>
     </div>
 </template>
 <script>
     import Card from './Card.vue'
-    export default {components : {Card},props : ['title','items']}
+    export default {
+    props : ['title','items'],
+    components : {Card},
+    methods: {
+        updateItems(newItem) {
+            const newItems = [...this.items.map(item => {
+                if (item.taskId !== newItem.taskId) return item;
+                return newItem;
+            })];
+            this.$emit('updateItems', newItems);
+        }
+    }
+    }
 </script>
