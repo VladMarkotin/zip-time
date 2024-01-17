427b7d5a31c7e998262a3e50bc654a09e9376e55
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-01-17 17:40:05 +0300

    add controllLoading function

--- resources/js/components/dialogs/AddNotes/AddNotes.vue
+++ resources/js/components/dialogs/AddNotes/AddNotes.vue
@@ -19,69 +19,16 @@
 				</span>
 			</div>
 		</template>
-		<v-card
-		width="400"
-        >
-				<v-card-title>Notes list</v-card-title>
-				<!-- <v-divider></v-divider>
-				<v-card-text style="height: 300px;">
-					<template>
-						<div class="d-flex align-center flex-column">
-								<v-card
-								width="800"
-								title="This is a title"
-								subtitle="This is a subtitle"
-								text="This is content"
-								v-model="notesList"
-								v-for="(item, i) in notesList"
-      							:key="i"
-								>
-								<v-card-title >
-         								Note from {{ item.created_at }}
-											</v-card-title>
-								<v-card-text class="bg-white text--primary">
-									<b>
-										{{ item.note }}
-									</b>
-									<v-checkbox
-										v-model="ex4[i]"
-										label="red"
-										color="red"
-										value="red"
-										hide-details
-									></v-checkbox>
-								</v-card-text>
-								<v-divider v-if="item.created_at == new Date('d.m.Y')"></v-divider>
-
-							</v-card>
-							<span v-if="noteInfo.todayAmount == 0">
-								There are no any notes
-							</span>
-						</div>
-					</template>
-				</v-card-text>
-				<v-divider></v-divider>
-				<v-card-actions>
-				<v-btn
-					color="blue-darken-1"
-					variant="text"
-					@click="dialogNotes = false"
-				>
-					Close
-				</v-btn>
-				<v-btn
-					color="blue-darken-1"
-					variant="text"
-					@click="dialogNotes = false"
-				>
-					Save
-				</v-btn>
-				</v-card-actions> -->
-			</v-card>
-			</v-dialog> 
+		<template v-if="isShowNotesDialog">
+            <AddNotesCard 
+            :isLoading = "isLoading"
+            />
+        </template>
+	</v-dialog> 
 </template>
 
 <script>
+import AddNotesCard from './AddNotesCard.vue';
 import { mdiNotebookEditOutline }  from '@mdi/js'
     export default {
         props: {
@@ -101,7 +48,12 @@ import { mdiNotebookEditOutline }  from '@mdi/js'
             return {
                 isShowNotesDialog: false,
                 icons: { mdiNotebookEditOutline},
+                isLoading: false,
+                minPreloaderDispTime: 500,
             }
+        },
+        components: {
+            AddNotesCard,
         },
         methods: {
 
@@ -111,14 +63,29 @@ import { mdiNotebookEditOutline }  from '@mdi/js'
             },
 
             getAllNotesForTask() {
+                const controllLoadingTime = (time, callback) => {
+				    if (time > this.minPreloaderDispTime) callback();
+				    else setTimeout(callback, this.minPreloaderDispTime - time);
+			    }
+
+                this.isLoading = true
+                const loadingStart = Date.now();
+
 				/**query for getting all notes */
 				axios.post('/get-saved-notes',{
                     task_id : this.item.taskId, 
                     hash:     this.item.hash
                 })
 				.then((response) => {
-                    const notesList = response.data;
-					this.updateNotesInfo({notesList}); 
+                    const loadingEnd = Date.now();
+
+                    controllLoadingTime(loadingEnd - loadingStart, () => {
+                        this.isLoading = false;
+
+                        const notesList = response.data;
+                        this.updateNotesInfo({notesList}); 
+                    })
+
 				})
 			},
 
--- resources/js/components/dialogs/AddNotes/AddNotesCard.vue
+++ resources/js/components/dialogs/AddNotes/AddNotesCard.vue
@@ -1,9 +1,78 @@
 <template>
-
+    <v-card
+	width="400"
+    >
+		<v-card-title>Notes list</v-card-title>
+		<!-- <v-divider></v-divider>
+			<v-card-text style="height: 300px;">
+				<template>
+					<div class="d-flex align-center flex-column">
+						<v-card
+						width="800"
+						title="This is a title"
+						subtitle="This is a subtitle"
+						text="This is content"
+						v-model="notesList"
+						v-for="(item, i) in notesList"
+      					:key="i"
+						>
+						<v-card-title >
+         					Note from {{ item.created_at }}
+						</v-card-title>
+						<v-card-text class="bg-white text--primary">
+							<b>
+								{{ item.note }}
+							</b>
+							<v-checkbox
+								v-model="ex4[i]"
+								label="red"
+								color="red"
+								value="red"
+								hide-details
+							></v-checkbox>
+						</v-card-text>
+						<v-divider v-if="item.created_at == new Date('d.m.Y')"></v-divider>
+						</v-card>
+						<span v-if="noteInfo.todayAmount == 0">
+							There are no any notes
+						</span>
+			        </div>
+			    </template>
+		    </v-card-text>
+				<v-divider></v-divider>
+				<v-card-actions>
+				<v-btn
+					color="blue-darken-1"
+					variant="text"
+					@click="dialogNotes = false"
+				>
+					Close
+				</v-btn>
+				<v-btn
+					color="blue-darken-1"
+					variant="text"
+					@click="dialogNotes = false"
+				>
+					Save
+				</v-btn>
+				</v-card-actions> -->
+	</v-card>
 </template>
 
 <script>
     export default {
-        
+        props: {
+            isLoading: {
+                type: Boolean,
+            },
+        },
+        data() {
+            return {
+
+            };
+        },
+        created() {
+            console.log(this.isLoading);
+        }
     }
 </script>
