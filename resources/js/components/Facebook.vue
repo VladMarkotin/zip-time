6099e726c63815a8de04785ae1b6c4f3bec0b515
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-01-24 17:44:19 +0300

    update facebook login button css

--- resources/js/components/Facebook.vue
+++ resources/js/components/Facebook.vue
@@ -1,7 +1,14 @@
 <template>
   <!--<v-facebook-login app-id="263292435338035"></v-facebook-login>-->
   <a href="/login/facebook " class="btn-facebook">
-    <button class="login-facebook-button"><span>Continue with</span></button>
+    <button class="login-facebook-button">
+      <div class="login-facebook-button__icon-wrapper">
+        <svg class="login-facebook-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" /></svg>
+      </div>
+      <div class="login-facebook-button__text-wrapper">
+        <span class="login-facebook-button__text">Continue with Facebook</span>
+      </div>
+    </button>
   </a>
 </template>
 
@@ -36,11 +43,18 @@
 </script>
 
 <style scoped>
+  .btn-facebook {
+    text-decoration: none;
+  }
+
+  .login-facebook-icon {
+    height: 35px;
+    width: 35px;
+    fill: #FFFFFF;
+  }
   .login-facebook-button {
     font-family: 'Inter', sans-serif;
-    font-weight: 400;
-    line-height: 20px;
-    padding: 0.55rem 1.55rem;
+    padding: 7px 17px;
     border-radius: 10rem;
     color: #fff;
     text-transform: uppercase;
@@ -49,7 +63,17 @@
     position: relative;
     overflow: hidden;
     z-index: 1;
-    text-shadow: 1px 1px 1px black;
+    display: flex;
+    align-items: center;
+  }
+
+  .login-facebook-button__text {
+    font-size: 13px;
+    margin: 0 7px;
+    font-weight: 400;
+    -webkit-text-shadow: -1px 0px 1px rgba(0, 0, 0, 0.39);
+    -moz-text-shadow: -1px 0px 1px rgba(0, 0, 0, 0.39);
+    text-shadow: -1px 0px 1px rgba(0, 0, 0, 0.39);
   }
 
   .login-facebook-button:after {
@@ -59,8 +83,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    /* background-color: #ff2b2b; было */
-    background-color: #de0000;
+    background-color: rgb(230, 0, 0);
     border-radius: 10rem;
     z-index: -2;
   }
@@ -72,8 +95,7 @@
     left: 0;
     width: 0%;
     height: 100%;
-    /* background-color: #de0000; было */
-    background-color: #ff2b2b;
+    background-color: #0074e0;
     transition: all 0.3s;
     border-radius: 10rem;
     z-index: -1;
