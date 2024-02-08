cde3cf84511773a58ffbdb0fc507799565d6b4fd
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-02-08 14:32:04 +0300

    update

--- resources/js/privacyPolicyPage/PrivacyPolicy.vue
+++ resources/js/privacyPolicyPage/PrivacyPolicy.vue
@@ -1,10 +1,72 @@
 <template>
-    <h1>1</h1>
+    <div class="inner-block">
+        <div class="privacy-policy-container">
+            <v-navigation-drawer 
+            width="330"
+            >
+                <v-list>
+                    <v-list-item v-for="(text, index) in text" :key="index" @click="scrollToSection(item)">
+                        <v-list-item-title>{{`${index + 1}. ${text.title}`}}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+            <div>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+                <p>112</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-
+        data() {
+            return {
+                text: [
+                    {title: 'Introduction'},
+                    {title: 'Interpretation and Definitions'},
+                    {title: 'Collecting and Using Your Personal Data'},
+                    {title: 'Children\'s Privacy'},
+                    {title: 'Links to Other Websites'},
+                    {title: 'Changes to this Privacy Policy'},
+                    {title: 'Contact Us'},
+                ]
+            }
+        }
     }
 </script>
 
