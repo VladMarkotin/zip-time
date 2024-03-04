a5b0c386b2488de3bcd687f2d42ed27de9621d32
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-03-04 16:38:35 +0300

    add ChallengeToggleButton component

---
+++ resources/js/components/UI/ChallengeToggleButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-btn 
+    v-on:click="$emit('toggleDispChallenges')"
+    outlined
+    color="red"
+    large
+    >
+		{{ buttonText.get(showChallenges) }}
+	</v-btn>
+</template>
+
+<script>
+    export default {
+        props: {
+            showChallenges: {
+                type: Boolean,
+                required: true,
+            }
+        },
+        data() {
+            return {
+                buttonText: new Map([
+                    [true, 'Close my Challenges'],
+                    [false, 'Show my Challenges']
+                ]),
+            }
+        }
+    }
+</script>
