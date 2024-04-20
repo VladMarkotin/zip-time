ab70bc19fb8465d964473635db23be59eaaf7aa9
Vlad Markotin <vlad.markotin@gmail.com>
2024-04-20 14:24:23 +0300

    edit comment

--- resources/js/components/cards/ClosedReadyDayPlan.vue
+++ resources/js/components/cards/ClosedReadyDayPlan.vue
@@ -19,7 +19,31 @@
 			</v-list-item>
 			<v-list-item>
 				<v-list-item-content class="key">Comment:</v-list-item-content>
-				<v-list-item-content class="align-end">{{data.comment}}</v-list-item-content>
+				<v-list-item-content class="align-end" v-if="!editCommentFlag">
+					{{data.comment}}
+				</v-list-item-content>
+				<v-list-item-content class="align-end" v-else>
+					<v-textarea
+						clear-icon="mdi-close-circle"
+						label="Describe your day"
+						v-model="newComment"
+						clearable
+					></v-textarea>
+				</v-list-item-content>
+				<v-list-item-content class="align-right"> 
+					<v-tooltip right>
+						<template v-slot:activator="{on}">
+							<v-btn icon v-on="on" v-on:click="editComment">
+								<v-icon color="#D71700"> {{icons.mdiPencil}} </v-icon>
+							</v-btn>
+							<v-btn icon v-on="on" v-on:click="editComment">
+								<v-icon color="#D71700"> {{icons.mdiContentSaveMoveOutline }} </v-icon>
+							</v-btn>
+							
+						</template>
+						<span>Prev day</span>
+					</v-tooltip>
+				</v-list-item-content>
 			</v-list-item>
 		</v-list>
 		<v-card-actions class="d-flex justify-space-between">
@@ -51,7 +75,7 @@
 	</v-card>
 </template>
 <script>
-	import {mdiArrowLeft,mdiCalendarToday,mdiArrowRight} from '@mdi/js'
+	import {mdiArrowLeft,mdiCalendarToday,mdiArrowRight, mdiPencil, mdiContentSaveMoveOutline } from '@mdi/js'
 	import Challenges from "./../challenges/Challenges.vue";
 
 	export default
@@ -60,7 +84,12 @@
 		components: { Challenges },
 		data()
 		{
-			return {date : new Date(),icons : {mdiArrowLeft,mdiCalendarToday,mdiArrowRight},disabled : {prevButton : false,nextButton : true}}
+			return {date : new Date(),
+				   icons : {mdiArrowLeft,mdiCalendarToday,mdiArrowRight, mdiPencil, mdiContentSaveMoveOutline },
+			       disabled : {prevButton : false,nextButton : true},
+				   editCommentFlag: false,
+				   newComment: '',
+		   }
 		},
 		methods :
 			{
@@ -74,6 +103,7 @@
 					this.data.comment = data.plans[strDate].comment
 					this.disabled.prevButton = data.histLength == 0 
 					this.disabled.nextButton = date.toEnStr() == new Date().toEnStr()
+					this.newComment = this.data.comment
 				},
 				getDayStatusName(statusCode)
 				{
@@ -84,6 +114,12 @@
 							'2' : 'In progress',
 							'3' : 'Success'
 						}[statusCode]
+				},
+				editComment () {
+					this.editCommentFlag = !this.editCommentFlag
+					console.log(this.newComment)
+					this.data.comment = this.newComment
+
 				}
 			}
 	}
