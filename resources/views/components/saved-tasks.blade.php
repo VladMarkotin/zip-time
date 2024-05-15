7f3ca55fb3c6a4c3fda8da39f3110716bfd88d51
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-05-15 13:07:09 +0300

    add horizontal scroll to the table

--- resources/views/components/saved-tasks.blade.php
+++ resources/views/components/saved-tasks.blade.php
@@ -1,51 +1,53 @@
 <div class="saved-task-container">
-    <div class="saved-task-table-wrapper">
-        <table class="table table-bordered border-primary saved-tasks-table">
-            <thead class="thead" style="background-color: #A10000;color:#ffffff">
-                <tr class="saved-tasks-table-head">
-                    <th scope="col">#</th>
-                    <th scope="col">Task name</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Priority</th>
-                    <th scope="col">Time</th>
-                    <th scope="col" style="text-align: center">Actions</th>
-                </tr>
-            </thead>
-            @foreach ($savedTasks as $sT)
-                <tr @if($sT->status == 0) class="disabled-saved-task" @endif>
-                    <td class="col-1 saved-task-code">{{ $sT->hash_code }}</td>
-                    <td class="col-4 saved-task-name">{{ $sT->task_name }}</td>
-                    <td class="col-1 saved-task-type">{{ $sT->type }}</td>
-                    <td class="col-1 saved-task-priority">{{ $sT->priority }}</td>
-                    <td class="col-1 saved-task-time">{{ $sT->time }}</td>
-                    <td class="col-4" >
-                        <div class="buttons-wrapper">
-                            <div>
-                                <button class="btn btn-outline-primary" data-toggle="modal" data-target="#infoModal"
-                                    wire:click="getInfo({{ $sT['id'] }})">
-                                    Info
-                                </button>
+    <div class="table-container">
+        <div class="saved-task-table-wrapper">
+            <table class="table table-bordered border-primary saved-tasks-table">
+                <thead class="thead" style="background-color: #A10000;color:#ffffff">
+                    <tr class="saved-tasks-table-head">
+                        <th scope="col">#</th>
+                        <th scope="col">Task name</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Priority</th>
+                        <th scope="col">Time</th>
+                        <th scope="col" style="text-align: center">Actions</th>
+                    </tr>
+                </thead>
+                @foreach ($savedTasks as $sT)
+                    <tr @if($sT->status == 0) class="disabled-saved-task" @endif>
+                        <td class="col-1 saved-task-code">{{ $sT->hash_code }}</td>
+                        <td class="col-4 saved-task-name">{{ $sT->task_name }}</td>
+                        <td class="col-1 saved-task-type">{{ $sT->type }}</td>
+                        <td class="col-1 saved-task-priority">{{ $sT->priority }}</td>
+                        <td class="col-1 saved-task-time">{{ $sT->time }}</td>
+                        <td class="col-4" >
+                            <div class="buttons-wrapper">
+                                <div>
+                                    <button class="btn btn-outline-primary" data-toggle="modal" data-target="#infoModal"
+                                        wire:click="getInfo({{ $sT['id'] }})">
+                                        Info
+                                    </button>
+                    
+                                    <button class="btn btn-outline-primary" data-toggle="modal"
+                                        data-target="#noteModal" wire:click="getNote({{ $sT['id'] }})">
+                                        Notes
+                                    </button>
+                    
+                                    <button class="btn btn-outline-primary" data-toggle="modal"
+                                        data-target="#updateModal" wire:click="edit({{ $sT['id'] }})">
+                                        Edit
+                                    </button>
+                                </div>
                 
-                                <button class="btn btn-outline-primary" data-toggle="modal"
-                                    data-target="#noteModal" wire:click="getNote({{ $sT['id'] }})">
-                                    Notes
-                                </button>
-                
-                                <button class="btn btn-outline-primary" data-toggle="modal"
-                                    data-target="#updateModal" wire:click="edit({{ $sT['id'] }})">
-                                    Edit
-                                </button>
+                                @if (!$sT->status)
+                                    <button wire:click="destroy({{ $sT['id'] }})" class="btn btn-outline-secondary" style="min-width: 75px">Enable</button>
+                                @else
+                                    <button wire:click="destroy({{ $sT['id'] }})" class="btn btn-danger" style="min-width: 75px">Disable</button>
+                                @endif
                             </div>
-            
-                            @if (!$sT->status)
-                                <button wire:click="destroy({{ $sT['id'] }})" class="btn btn-outline-secondary" style="min-width: 75px">Enable</button>
-                            @else
-                                <button wire:click="destroy({{ $sT['id'] }})" class="btn btn-danger" style="min-width: 75px">Disable</button>
-                            @endif
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 </div>
