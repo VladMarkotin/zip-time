2a1de729b0d8f0279bcb1c1ce46e4a508b510abd
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-06-04 18:27:09 +0300

    upadte cards type icons

---
+++ resources/js/components/UI/CardsTypeImage.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-tooltip right>
+        <template v-slot:activator="{on}">     
+            <v-img 
+                v-on="on" 
+                max-height="128"
+                max-width="128"
+                class="card_type_img"
+                :src="imageSrc"
+                />
+        </template>
+        <span>{{title}}</span>
+    </v-tooltip>
+</template>
+
+<script>
+    export default {
+        props : {
+            title: {
+                type: String,
+            }
+        },
+        computed: {
+            imageSrc() {
+                return this.cardsTypeImageMap.get(this.title);
+            }
+        },
+        data() {
+           const cardsTypeImageMap = new Map;
+
+           cardsTypeImageMap.set('Required tasks:', '/images/cards_required_image.png')
+           cardsTypeImageMap.set('Non required tasks:', '/images/cards_nonRequired_image.png')
+
+            return {
+                cardsTypeImageMap,
+            }
+        }
+    }
+</script>
