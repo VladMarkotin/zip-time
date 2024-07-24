9e1f1dda59995308a77425b594593e1f53b73a55
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-07-24 14:32:50 +0300

    update

--- resources/js/components/dialogs/EmergencyCall.vue
+++ resources/js/components/dialogs/EmergencyCall.vue
@@ -6,70 +6,74 @@
 	>
 		<v-card>
 			<v-card-title class="font-weight-bold v-card-title">Emergency call</v-card-title>
-			<v-card-text>
-				<v-container>
-					<v-row>
-						<v-col cols="12" sm="7">
-							<v-date-picker 
-							v-model="dates"
-							color="#A10000"
-							range 
-							:min="minDate"
-							:max="maxDate"
-							>
-							</v-date-picker>
-						</v-col>
-						<v-col cols="12" sm="5">
-							<v-text-field 
-							class="input-data-period"
-							v-model="dateRangeText"
-							label="Date period" 
-							prepend-icon="mdi-calendar" 
-							append-icon="mdi-close-circle" 
-							@click:append="resetDates"
-							readonly 
-							>
-						</v-text-field>
-						</v-col>
-					</v-row>
-					<v-row>
-						<v-col cols="12">
-							<v-textarea counter="256" label="Comment" rows="2" outlined shaped v-model="comment" style="margin-top : -25px"></v-textarea>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-card-text>
-			<v-divider></v-divider>
-			<v-card-actions class="justify-space-between v-card-actions emergencyCall_emergencyCall-footer">
-				<v-tooltip right>
-					<template v-slot:activator="{on}">
-						<v-btn 
-							icon 
-							v-on="on" 
-							v-on:click="callEmergency"
-							:disabled = "!isDateSet"
-							>
-							<v-icon color="#D71700" large>{{icons.mdiCarEmergency}}</v-icon>
-						</v-btn>
-					</template>
-					<span>Emergency call</span>
-				</v-tooltip>
-				<v-tooltip right>
-					<template v-slot:activator="{on}">
-						<v-btn icon v-on="on" v-on:click="toggle">
-							<v-icon color="#D71700" large>{{icons.mdiCancel}}</v-icon>
-						</v-btn>
-					</template>
-					<span>Cancel</span>
-				</v-tooltip>
-			</v-card-actions>
+				<v-card-text v-if="!isShowEmergencyConfirmation">
+					<v-container>
+						<v-row>
+							<v-col cols="12" sm="7">
+								<v-date-picker 
+								v-model="dates"
+								color="#A10000"
+								range 
+								:min="minDate"
+								:max="maxDate"
+								>
+								</v-date-picker>
+							</v-col>
+							<v-col cols="12" sm="5">
+								<v-text-field 
+								class="input-data-period"
+								v-model="dateRangeText"
+								label="Date period" 
+								prepend-icon="mdi-calendar" 
+								append-icon="mdi-close-circle" 
+								@click:append="resetDates"
+								readonly 
+								>
+							</v-text-field>
+							</v-col>
+						</v-row>
+						<v-row>
+							<v-col cols="12">
+								<v-textarea counter="256" label="Comment" rows="2" outlined shaped v-model="comment" style="margin-top : -25px"></v-textarea>
+							</v-col>
+						</v-row>
+					</v-container>
+				</v-card-text>
+				<EmergencyConfirmation 
+				v-else
+				:emergencyModeDates="dates"
+				@goBackOneStep="isShowEmergencyConfirmation = false"
+				/>
+				<v-divider></v-divider>
+				<v-card-actions class="justify-space-between v-card-actions emergencyCall_emergencyCall-footer">
+					<v-tooltip right>
+						<template v-slot:activator="{on}">
+							<v-btn 
+								icon 
+								v-on="on" 
+								v-on:click="callEmergency"
+								:disabled = "!isDateSet"
+								>
+								<v-icon color="#D71700" large>{{icons.mdiCarEmergency}}</v-icon>
+							</v-btn>
+						</template>
+						<span>Emergency call</span>
+					</v-tooltip>
+					<v-tooltip right>
+						<template v-slot:activator="{on}">
+							<v-btn icon v-on="on" v-on:click="toggle">
+								<v-icon color="#D71700" large>{{icons.mdiCancel}}</v-icon>
+							</v-btn>
+						</template>
+						<span>Cancel</span>
+					</v-tooltip>
+				</v-card-actions>
 		</v-card>
 	</v-dialog>
 </template>
 <script>
 	import {mdiCarEmergency,mdiCancel} from '@mdi/js'
 	import EmergencyConfirmation from './EmergencyConfirmation.vue';
-	import { watch } from 'vue';
 	export default
 		{
 			data()
@@ -87,6 +91,7 @@
 					minDate: today.toISOString(),
 					daysAllowedinOrder: daysAllowedinOrder,
 					maxDate:  maxDate,
+					isShowEmergencyConfirmation: false,
 				}
 			},
 			components: {EmergencyConfirmation},
@@ -127,6 +132,8 @@
 			{
 				callEmergency()
 				{
+					this.isShowEmergencyConfirmation = true;
+					return;
 					const today = (new Date()).toEnStr();
 					axios.post('/emergency',{from : this.dates[0],to : this.dates[1],comment : this.comment})
 					.then(response => {
--- resources/js/components/dialogs/EmergencyConfirmation.vue
+++ resources/js/components/dialogs/EmergencyConfirmation.vue
@@ -1,9 +1,87 @@
 <template>
-    <h1>Test</h1>
+    <div class="emergency-confirmation-container">
+        <v-card-text>
+            <v-container>
+                <v-tooltip right>
+						<template v-slot:activator="{on}">
+							<v-btn 
+								icon 
+								v-on="on" 
+								@click="$emit('goBackOneStep')"
+								>
+								<v-icon color="#D71700" large>{{icons.mdiStepBackward}}</v-icon>
+							</v-btn>
+						</template>
+						<span>Go back one step</span>
+					</v-tooltip>
+                    <v-card-title class="emergency-confirmation-title"><p><span class="customRed">Attention!</span> You are about to activate <br/> <span class="customRed">«Emergency mode»</span> !</p></v-card-title>
+                    <div style="display: flex; justify-content: center;">
+                        <v-card-text class="emergency-confirmation-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aliquam assumenda hic quia labore odit quis ipsam quas ab iure accusantium architecto exercitationem magni eius commodi at, laudantium, totam velit alias voluptatibus recusandae unde odio ratione. Inventore, libero?</v-card-text>
+                    </div>
+                    <p>{{ emergencyConfirmationTolltip }}</p>
+            </v-container>
+        </v-card-text>
+    </div>
 </template>
 
 <script>
+import {mdiStepBackward} from '@mdi/js'
     export default {
+        props: {
+            emergencyModeDates: {
+                type: Array,
+            },
+        },
+        data() {
+            return {
+                icons : {mdiStepBackward}, 
+            }
+        },
+        computed: {
+            emergencyConfirmationTolltip() {
+                const formatedDates = this.emergencyModeDates.map(date => {
+                    let parts = date.split('-'); 
+                    return `${parts[2]}.${parts[1]}.${parts[0]}`;
+                });
 
+                let text = 'If you are sure that you want to activate Emergency Mode '
+
+                if (formatedDates.length === 1) {
+                    text += `on ${formatedDates[0]}`;
+                } else {
+                    text += `from the ${formatedDates[0]} to the ${formatedDates[1]}`;
+                }
+                console.log(text);
+                return ''
+            }
+        }
     }
 </script>
+
+<style scoped>
+    .emergency-confirmation-container {
+        min-height: 515px;
+    }
+
+    .emergency-confirmation-title {
+        text-align: center;
+        font-size: 26px;
+        justify-content: center;
+        font-weight: 600;
+    }
+
+    .emergency-confirmation-title p {
+        margin-bottom: 0 !important;
+    }
+
+    .customRed {
+        color: #D71700 !important;
+    }
+
+    .emergency-confirmation-text {
+        max-width: 80% !important;
+        text-align: justify;
+        font-size: 16px;
+        padding: 0;
+    }
+</style>
