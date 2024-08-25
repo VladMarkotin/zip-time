bebe81e31fe76b9faff5677f8a7e33d8b009dd4f
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-08-25 13:32:23 +0300

    add add_task_id_to_notes_table migration

--- database/migrations/2024_05_11_195336_add_device_token_to_users_table.php
+++ database/migrations/2024_05_11_195336_add_device_token_to_users_table.php
@@ -26,7 +26,7 @@ class AddDeviceTokenToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('device_token');
         });
     }
 }
