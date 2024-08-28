da9e6103ea36d4686da35baafcea35b2c9823ace
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-08-28 18:10:08 +0300

    add content to the dataDeletionPolicy page

--- resources/js/dataDeletionPolicyPage/DataDeletionPolicy.vue
+++ resources/js/dataDeletionPolicyPage/DataDeletionPolicy.vue
@@ -1,5 +1,52 @@
 <template>
-    <v-list>Data Deletion Policy</v-list>
+    <div class="inner-block">
+        <v-card class="p-3">
+            <section class="dataDeletionPolicy-section pt-0">
+                <v-card-title 
+                tag="h1"  
+                class="dataDeletionPolicy-title dataDeletionPolicy-main-title">Data Deletion Policy</v-card-title>
+                <v-card-title 
+                tag="h2" 
+                class="dataDeletionPolicy-title dataDeletionPolicy-section-title"> 
+                    How to request data deletion
+                </v-card-title>
+                <v-card-text class="pt-0">If you wish to request the deletion of your data from <a class="dataDeletionPolicy-link" href="/">quipl.co</a>, please send an email to our address: <a class="dataDeletionPolicy-link" href="mailto:support@quipl.co">support@quipl.co</a> or fill out the following data deletion request form.</v-card-text>
+            </section>
+            <section class="dataDeletionPolicy-section pt-0">
+                <v-card-title 
+                tag="h2" 
+                class="dataDeletionPolicy-title dataDeletionPolicy-section-title"> 
+                    What data will be deleted
+                </v-card-title>
+                <v-card-text class="pt-0">Upon request for data deletion from our site, the following data will be deleted:</v-card-text>
+                <ul class="dataDeletionPolicy-list">
+                    <li class="dataDeletionPolicy-li">- Profile data</li>
+                    <li class="dataDeletionPolicy-li">- Plans</li>
+                    <li class="dataDeletionPolicy-li">- Tasks</li>
+                </ul>
+            </section>
+            <section class="dataDeletionPolicy-section pt-0">
+                <v-card-title 
+                tag="h2" 
+                class="dataDeletionPolicy-title dataDeletionPolicy-section-title"> 
+                    Data deletion timeframe
+                </v-card-title>
+                <v-card-text class="pt-0">Your data will be completely deleted within 30 days of receiving your request.</v-card-text>
+            </section>
+            <section class="dataDeletionPolicy-section pt-0">
+                <v-card-title 
+                tag="h2" 
+                class="dataDeletionPolicy-title dataDeletionPolicy-section-title"> 
+                    Contact information for questions
+                </v-card-title>
+                <v-card-text class="pt-0">If you have any questions regarding data deletion, please contact us at: support@quipl.co.</v-card-text>
+            </section>
+            <section class="dataDeletionPolicy-section pt-0">
+                <v-card-text class="pt-0">Thank you</v-card-text>
+                <v-card-text class="pt-0">The <a class="dataDeletionPolicy-link" href="/">quipl.co</a> Team</v-card-text>
+            </section>
+        </v-card>
+    </div>
 </template>
 
 <script>
