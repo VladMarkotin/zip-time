c3913ae15bf34b5cd575fc2ee9e66c29ae001d42
Vlad <vlad.markotin@gmail.com>
2024-09-02 20:55:28 +0300

    add google auth2

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_URL'),
+        'redirect'      => env('GOOGLE_URL'),
     ],
 
     'gpt' => [
