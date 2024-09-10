bfeb4137f09c83da9190d86c2b7d0d55d92c009f
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-09-10 17:39:15 +0300

    update

--- resources/js/components/dialogs/ClosedDayCommentDialog.vue
+++ resources/js/components/dialogs/ClosedDayCommentDialog.vue
@@ -93,6 +93,7 @@
         overflow-y: scroll;
         border-radius: 10px;
         border:  1px solid #e0e0e0;
+        word-break: break-word;
     }
 
     .comment-text-field::-webkit-scrollbar {
