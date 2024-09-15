f9c7bd75335d9c14928446e62338f4d625f0a35e
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-09-16 00:07:41 +0300

    update

--- resources/js/components/cards/Plan/Plan.vue
+++ resources/js/components/cards/Plan/Plan.vue
@@ -113,21 +113,11 @@
                <v-col md="2"
                class="plan_taskType-input-wrapper"
                >
-                  <v-select
-                     :label="placeholders[1]"
-                     required
-                     :items="taskTypes"
-                     v-model="defaultSelected.type"
-                     >
-                     <template v-slot:item="{item}" >
-                           <v-list-item >{{ item }}</v-list-item>
-                           <!-- open-on-hover -->
-                           <VSelectTooptip 
-                           :item="item"
-                           :tooltipData = "tooltipTypesData"
-                           />
-                     </template>
-                  </v-select>
+                  <SelectTaskType 
+                  :label="placeholders[1]"
+                  :taskTypes="taskTypes"
+                  v-model="defaultSelected.type"
+                  />
                </v-col>
                <v-col 
                md="1"
@@ -320,6 +310,7 @@ import EmergencyCall from '../../dialogs/EmergencyMode/EmergencyCall.vue';
 import PreplanTasksTable from './PreplanTasksTable.vue';
 import VSelectTooptip from '../../UI/VSelectTooptip.vue';
 import CustomTimepicker from '../../UI/CustomTimepicker.vue';
+import SelectTaskType from '../../UI/SelectTaskType.vue';
 import {
     mdiAccount,
     mdiPlex,
@@ -338,7 +329,16 @@ import { uuid } from 'vue-uuid';
 import createWatcherForDefSavTaskMixin from '../../../mixins';
 
 export default {
-   components : {EmergencyCall, AddHashCode, AddHashCodeButton, CleanHashCodeButton, PreplanTasksTable, VSelectTooptip, CustomTimepicker},
+   components : {
+      EmergencyCall, 
+      AddHashCode, 
+      AddHashCodeButton, 
+      CleanHashCodeButton, 
+      PreplanTasksTable, 
+      VSelectTooptip, 
+      CustomTimepicker,
+      SelectTaskType,
+   },
    mixins: [createWatcherForDefSavTaskMixin('defaultSelected.hash')],
     data: () => ({
          placeholders: ['Enter name of task here', 'Type', 'Priority', 'Time', 'Details', 'Notes'],
@@ -456,23 +456,6 @@ export default {
                : ['required job', 'non required job', 'required task', 'task']
         },
 
-        tooltipTypesData() {
-            return {
-               titles: {
-                  requiredJob: 'required job',
-                  nonRequiredJob: 'non required job',
-                  requiredTask: 'required task',
-                  task: 'task',
-               },
-               descriptions: {
-                  requiredJob: `<span style="text-indent: -1em; padding-left: 1em;">Main entity of your plan. By adding a required job, you kind of sign a commitment with yourself that you will at least start doing it today. After finishing working on a job, you should evaluate the effort you spent.</span><br><span style="text-indent: -1em; padding-left: 1em;">By 23:59 all (!) required jobs should be evaluated</span>`,
-                  nonRequiredJob: `<span style="text-indent: -1em; padding-left: 1em;">Some of your day\`s jobs could be not so important but, anyway, you want to evaluate your efforts. So, non required jobs are exactly what you need. Failure to comply has no consequences. Moreover, to complete all non required jobs is a perfect way to increase your final day grade</span>`,
-                  requiredTask: `<span style="text-indent: -1em; padding-left: 1em;">We often have a plenty of small (but important) tasks (e.g “call to the boss”). It would be difficult to evaluate the result of such task, but, anyway, you have to do it.</span>`,
-                  task: `<span style="text-indent: -1em; padding-left: 1em;">Anything that\`s not so important and hard to evaluate but necessary personally for you ( e.g. “night out with friends” )</span>`,
-               },
-            }
-         },
-
          tooltipPrioritiesData() {
             return {
                titles: {
@@ -499,7 +482,8 @@ export default {
 
          isDefaultSavedTaskSelected() {
             return !!this.defaultSelected.hash.match(/^#q-/);
-         }
+         },
+
     },
     methods: {
       ...mapMutations(['SET_WINDOW_SCREEN_WIDTH']),
@@ -638,17 +622,17 @@ export default {
 
          this.items.push({...this.defaultSelected, uniqKey: this.generateUniqKey()});
          this.showIcon = 0;
-         const self = this;
          this.defaultSelected = {
-                hashCodes: this.defaultSelected.hashCodes,
-                hash: '#',
-                taskName: '',
-                time: '01:00',
-                type: self.currentDayDefaultTaskType,
-                priority: '1',
-                details: '',
-                notes: ''
-            }
+               ...this.defaultSelected,
+               hashCodes: this.defaultSelected.hashCodes,
+               hash: '#',
+               taskName: '',
+               time: '01:00',
+               type: this.currentDayDefaultTaskType,
+               priority: '1',
+               details: '',
+               notes: ''
+         }
             // если показываю алерт через метод setAlert и использую название таски, то важно для регулярки что бы текст был вида
             // The task .... has  (влияет на работу регулярки и обрезание большого названия таски)
          this.setAlert({serverMessage: `The task ${taskName} has been successfully added`, alertType: 'success'});
@@ -736,6 +720,10 @@ export default {
       updateScreenWidth() {
          this.SET_WINDOW_SCREEN_WIDTH({windowScreenWidth: window.innerWidth});
       },
+
+      setTaskType(value) {
+         console.log(value);
+      }
     },
     created() {
         let currentObj = this;
--- resources/js/components/dialogs/AddJobTask.vue
+++ resources/js/components/dialogs/AddJobTask.vue
@@ -64,20 +64,12 @@
 							class="p-0 m-0"
 							></v-col>
 							<v-col class="p-0 m-0">
-								<v-select 
-								label="Type" 
-								v-bind:items="types" 
-								v-model="task.type"
-								@change="compareWithTemplate"
-								>
-								<template v-slot:item="{item}">
-									<v-list-item >{{ item }}</v-list-item>
-									<VSelectTooptip 
-                          			:item        = "item"
-									:tooltipData = "tooltipTypesData"
-                           			/>
-								</template>
-							</v-select>
+								<SelectTaskType 
+								label      = "Type" 
+								v-model    = "task.type"
+								:taskTypes = "types"
+								@change = "compareWithTemplate"
+								/>
 							</v-col>
 							<v-col
 							cols="1"
@@ -100,7 +92,6 @@
 								>
 								<template v-slot:item="{item}" >
 									<v-list-item >{{ item }}</v-list-item>
-									<!-- open-on-hover -->
 									<VSelectTooptip 
 									:item              = "String(item)"
 									:width             = "200"
@@ -159,12 +150,21 @@
 	import VSelectTooptip from '../UI/VSelectTooptip.vue';
 	import {mdiPlusBox, mdiClockTimeFourOutline} from '@mdi/js';
 	import CustomTimepicker from '../UI/CustomTimepicker.vue';
+	import SelectTaskType from '../UI/SelectTaskType.vue';
 
 	import createWatcherForDefSavTaskMixin from '../../mixins';
 
 	export default
 		{
-			components : {AddHashCode, AddHashCodeButton, CleanHashCodeButton, CloseButton, VSelectTooptip, CustomTimepicker},
+			components : {
+				AddHashCode, 
+				AddHashCodeButton, 
+				CleanHashCodeButton, 
+				CloseButton, 
+				VSelectTooptip, 
+				CustomTimepicker, 
+				SelectTaskType
+			},
 			props: {
 				dayStatus: {
 					type: Number,
@@ -210,23 +210,6 @@
 					}
 			},
 			computed: {
-				tooltipTypesData() {
-					return {
-						titles: {
-							requiredJob: 'required job',
-							nonRequiredJob: 'non required job',
-							requiredTask: 'required task',
-							task: 'task',
-						},
-						descriptions: {
-							requiredJob: `<span style="text-indent: -1em; padding-left: 1em;">Main entity of your plan. By adding a required job, you kind of sign a commitment with yourself that you will at least start doing it today. After finishing working on a job, you should evaluate the effort you spent.</span><br><span style="text-indent: -1em; padding-left: 1em;">By 23:59 all (!) required jobs should be evaluated</span>`,
-							nonRequiredJob: `<span style="text-indent: -1em; padding-left: 1em;">Some of your day\`s jobs could be not so important but, anyway, you want to evaluate your efforts. So, non required jobs are exactly what you need. Failure to comply has no consequences. Moreover, to complete all non required jobs is a perfect way to increase your final day grade</span>`,
-							requiredTask: `<span style="text-indent: -1em; padding-left: 1em;">We often have a plenty of small (but important) tasks (e.g “call to the boss”). It would be difficult to evaluate the result of such task, but, anyway, you have to do it.</span>`,
-							task: `<span style="text-indent: -1em; padding-left: 1em;">Anything that\`s not so important and hard to evaluate but necessary personally for you ( e.g. “night out with friends” )</span>`,
-						},
-					}
-        		},
-
 				tooltipPrioritiesData() {
 					return {
 						titles: {
@@ -384,7 +367,6 @@
 				},
 
 				compareWithTemplate() {
-
 					if (this.task.hashCode !== '#') {
 						
 						const convertToJSON = (targetObj) => {
