e94856ce8ae1e6fec2a3ca0c60515bd47aa87790
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-10-04 14:32:48 +0300

    add Snackbar.vue component

---
+++ resources/js/components/UI/Snackbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-snackbar 
+    :value="value" 
+    @input="updateValue"
+    :timeout="5e3"
+    >
+        {{text}}
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: Boolean,
+                default: false,
+            },
+            text: {
+                type: String,
+                default: '',
+            }
+        },
+        methods: {
+            updateValue(value) {
+                this.$emit('input', value);
+            }
+        }
+    }
+</script>
