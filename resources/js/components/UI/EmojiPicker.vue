4b544d531a63040f0f24c19f7c1753bdb1dbebaf
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-10-26 01:57:42 +0300

    update

--- resources/js/components/UI/EmojiPicker.vue
+++ resources/js/components/UI/EmojiPicker.vue
@@ -1,13 +1,12 @@
 <template>
     <v-menu v-model="emojiPickerMenu" offset-y>
         <template #activator="{ on }">
-            <span @click="emojiPickerMenu = true">{{ selectedEmoji }}</span>
-            <!-- <v-img 
+            <v-img 
             @click="emojiPickerMenu = true"
             :src="`/images/marks/${selectedEmoji}`"  
             max-width="80px" 
             max-height="80px" 
-            /> -->
+            />
         </template>
         <v-card 
         class="p-2 m-0 d-flex flex-column align-items-center" 
@@ -16,17 +15,23 @@
         v-model="selectedEmoji" 
         @change="sendMark" 
         >
-            <v-tab 
-            v-for="(data, idx) in emojiTabsData"
-            :key="idx"
-            :tab-value = "data.iconName"
-            >
-                    <v-img 
-                    :src="`/images/marks/${data.iconName}`"  
-                    max-width="40px" 
-                    max-height="40px" 
-                    />
-            </v-tab>
+            <v-tooltip 
+            bottom v-for="(data, idx) in emojiTabsData" 
+            :key="idx">
+                <template v-slot:activator="{ on: tooltip  }">
+                    <v-tab 
+                    :tab-value = "data.iconName"
+                    v-on="tooltip"
+                    >
+                        <v-img 
+                        :src="`/images/marks/${data.iconName}`"  
+                        max-width="40px" 
+                        max-height="40px" 
+                        />
+                    </v-tab>
+                </template>
+                <span>{{ data.toopltipText }}</span>
+            </v-tooltip>
             <v-tabs-slider color="red"></v-tabs-slider>
         </v-tabs>
         </v-card>
@@ -49,9 +54,9 @@
         computed: {
             emojiTabsData() {
                 return [
-                    {iconName: 'sad_mark_icon.svg', value: 0, status: 'bad'},
-                    {iconName: 'sad2_mark_icon.svg', value: 0, status: 'normal'},
-                    {iconName: 'happy_mark_icon.svg', value: 99, status: 'good'},
+                    {iconName: 'sad_mark_icon.svg', value: 0, toopltipText: 'bad'},
+                    {iconName: 'sad2_mark_icon.svg', value: 0, toopltipText: 'normal'},
+                    {iconName: 'happy_mark_icon.svg', value: 99, toopltipText: 'good'},
                 ];
             },
             getInitialSelectedEmojiVal() { //вытащу из стора
