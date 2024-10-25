21bfc4adacb04afafb92c613bb489537298d1a62
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-10-26 02:14:31 +0300

    update

--- resources/js/components/UI/EmojiPicker.vue
+++ resources/js/components/UI/EmojiPicker.vue
@@ -1,12 +1,20 @@
 <template>
-    <v-menu v-model="emojiPickerMenu" offset-y>
+    <v-menu v-model="emojiPickerMenu" offset-y transition="scale-transition">
         <template #activator="{ on }">
-            <v-img 
-            @click="emojiPickerMenu = true"
-            :src="`/images/marks/${selectedEmoji}`"  
-            max-width="80px" 
-            max-height="80px" 
-            />
+            <v-tooltip right>
+                <template v-slot:activator="{ on: tooltip  }">
+                    <div class="selected_emoji_icon">
+                        <v-img 
+                        @click="emojiPickerMenu = true"
+                        :src="`/images/marks/${selectedEmoji}`"  
+                        v-on="tooltip"
+                        max-width="80px" 
+                        max-height="80px" 
+                        />
+                    </div>
+                </template>
+                <span>Set mark</span>
+            </v-tooltip>
         </template>
         <v-card 
         class="p-2 m-0 d-flex flex-column align-items-center" 
@@ -16,8 +24,10 @@
         @change="sendMark" 
         >
             <v-tooltip 
-            bottom v-for="(data, idx) in emojiTabsData" 
-            :key="idx">
+            bottom 
+            v-for="(data, idx) in emojiTabsData" 
+            :key="idx"
+            >
                 <template v-slot:activator="{ on: tooltip  }">
                     <v-tab 
                     :tab-value = "data.iconName"
@@ -76,3 +86,15 @@
         }
     }
 </script>
+
+<style scoped>
+    .selected_emoji_icon {
+        cursor: pointer;
+        transition: all .3s ease;
+        overflow: hidden;
+    }
+
+    .selected_emoji_icon:hover {
+        transform: scale(1.05) rotate(5deg); 
+    }
+</style>
