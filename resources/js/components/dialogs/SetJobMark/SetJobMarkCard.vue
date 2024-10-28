35da91755f0fd8563adf664b0b6f0b3805cf46bc
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2024-10-28 14:08:20 +0300

    update

--- resources/js/components/dialogs/SetJobMark/SetJobMarkCard.vue
+++ resources/js/components/dialogs/SetJobMark/SetJobMarkCard.vue
@@ -4,18 +4,21 @@
             How successfully was the task completed?
         </v-card-title>
         <v-card-text>
-            <v-slider
-            vertical
-            v-model="test"
-            :tick-labels="ticksLabels"
-            track-color="grey"
-            color="red darken-3"
-            :min="0"
-            :max="99"
-            :step="33"
-            ticks="always"
-            hide-details
-            ></v-slider>
+            <v-range-slider
+                :tick-labels="seasons"
+                :value="[0,1]"
+                min="0"
+                max="3"
+                ticks="always"
+                tick-size="4"
+            >
+                <template v-slot:thumb-label="props">
+                    <v-img src="/images/marks/bad_mark_icon.svg"  max-width="40px" 
+                    max-height="40px" >
+                        
+                    </v-img>
+                </template>
+            </v-range-slider>
         </v-card-text>
     </v-card>
 </template>
@@ -25,9 +28,25 @@
         data() {
             return {
                 test: 0,
-                ticksLabels: ['Bad', 'Normal.', 'Better.', 'Best.'],
+                seasons: [
+                'Winter',
+                'Spring',
+                'Summer',
+                'Fall',
+                ],
+                icons: [
+                    'mdi-snowflake',
+                    'mdi-leaf',
+                    'mdi-fire',
+                    'mdi-water',
+                ],
             }
         },
+        methods: {
+      season (val) {
+        return this.icons[val]
+      },
+    },
         created() {
             console.log('+++');
         }
